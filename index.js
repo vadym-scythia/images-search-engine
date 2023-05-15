c97b2fbdf3f4d10fb4c0338ce08e2cc269c2ea68
Vadym-Mandrichenko_bdgh <vadymmandrichenko@gmail.com>
2023-05-15 23:13:38 +0300

    Update to Web app

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+import express from 'express';
+import bodyParser from 'body-parser';
 import weaviate from 'weaviate-ts-client'
 import fs, { readFileSync, writeFileSync } from 'fs'
+
+const app = express();
+app.use(bodyParser.json({ limit: '50mb' }));
 
 const client = weaviate.client({
     scheme: 'http',
@@ -9,12 +14,17 @@ const client = weaviate.client({
 // for the first execution use here initialize_schema.js
 // for the first execution use here add_images_to_db.js
 
-const testImg = Buffer.from(readFileSync('.\\test.jpg')).toString('base64');
-const resultImg = await client.graphql.get()
-    .withClassName('Meme')
-    .withFields(['image'])
-    .withNearImage({ image : testImg })
-    .withLimit(1)
-    .do();
-const result = resultImg.data.Get.Meme[0].image;
-writeFileSync('.\\result.jpg', result, 'base64')
+app.post('/search', async (req, res) => {
+    console.log('hello from /search!');
+    const testImg = req.body.image;
+    const resultImg = await client.graphql.get()
+        .withClassName('Meme')
+        .withFields(['image'])
+        .withNearImage({ image: testImg })
+        .withLimit(1)
+        .do();
+    const result = resultImg.data.Get.Meme[0].image;
+    res.json({ image: result });
+});
+
+app.listen(3002, () => console.log('App listening on http://localhost:3002'));
